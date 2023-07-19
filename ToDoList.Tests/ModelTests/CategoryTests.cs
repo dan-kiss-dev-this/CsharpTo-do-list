34426965576ca869e38ecff8eeb1d322b46839ab
Daniel Kiss <flowfast47@gmail.com>
2023-07-19 15:56:52 -0700

    add testing and methods to the Category class

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -6,13 +6,61 @@ using System;
 namespace ToDoList.Tests
 {
     [TestClass]
-    public class CategoryTests
+    public class CategoryTests : IDisposable
     {
+        public void Dispose()
+        {
+            Category.ClearAll();
+        }
         [TestMethod]
         public void CategoryClass_MakeCategory_Category()
         {
-            Category testCategory = new Category();
-            Assert.AreEqual(testCategory.GetType(), typeof(Category));
+            string name = "Test Cateogry";
+            Category newCategory = new Category(name);
+            Assert.AreEqual(newCategory.GetType(), typeof(Category));
         }
+
+        [TestMethod]
+        public void GetName_ReturnsName_String()
+        {
+            string name = "Test Category";
+            Category newCategory = new Category(name);
+            Assert.AreEqual(newCategory.Name, name);
+        }
+
+        [TestMethod]
+        public void GetId_ReturnIds_Int()
+        {
+            int testId = 2;
+            string name = "Test Category";
+            string name2 = "Test Category2";
+            Category newCategory = new Category(name);
+            Category newCategory2 = new Category(name2);
+            Assert.AreEqual(newCategory2.Id, testId);
+        }
+
+        [TestMethod]
+        public void GetAll_GetAllCategories_ListOfCategory()
+        {
+            string name = "Test Category";
+            string name2 = "Test Category2";
+            Category newCategory = new Category(name);
+            Category newCategory2 = new Category(name2);
+            List<Category> expectedCategory = new List<Category> { newCategory, newCategory2 };
+            List<Category> allCategories = Category.GetAll();
+            CollectionAssert.AreEqual(allCategories, expectedCategory);
+        }
+
+        [TestMethod]
+        public void Find_GetCategoryById_Category()
+        {
+            string name = "Test Category";
+            string name2 = "Test Category2";
+            Category newCategory = new Category(name);
+            Category newCategory2 = new Category(name2);
+            Assert.AreEqual(Category.Find(2), newCategory2);
+        }
+
+
     }
 }
--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -7,10 +7,28 @@ namespace ToDoList.Models
         public static List<Category> _instances = new List<Category> { };
         public string Name { get; set; }
         public int Id { get; }
-        public List<Item> Items = new List<Item> { };
-        // public Category
-        // {
+        public List<Item> Items;
+        public Category(string name)
+        {
+            Name = name;
+            _instances.Add(this);
+            Id = _instances.Count;
+            Items = new List<Item> { };
+        }
 
-        // }
-}
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+
+        public static List<Category> GetAll()
+        {
+            return _instances;
+        }
+
+        public static Category Find(int id)
+        {
+            return _instances[id - 1];
+        }
+    }
 }
